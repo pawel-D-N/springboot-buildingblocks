acfdd05f8ba082efc5d829906010f73dd45dd19d
pawel-D-N <p..nicpon89@gmail.com>
2022-09-14 00:20:19 +0200

    JsonViews - First Commit

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -13,6 +13,7 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
 
 //import org.springframework.hateoas.ResourceSupport;
 
@@ -20,35 +21,44 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "users")
-@JsonFilter(value =  "userFilter")
+//@JsonIgnoreProperties({"firstname", "lastname"}) - Static filtering @JsonIgnore
+//@JsonFilter(value =  "userFilter") -- Used for MappingJacksonValue
 public class User /*extends ResourceSupport*/ {
 	
 	@Id()
 	@GeneratedValue
+	@JsonView(Views.External.class)
 	private Long userid;
 	
 	@NotEmpty(message = "Username is Mandatory field. Please provide username")
 	@Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
+	@JsonView(Views.External.class)
 	private String username;
 	
 	@Size(min = 2, message = "FirstName should have atleast 2 characters")
 	@Column(name = "FIRST_NAME", length = 50, nullable = false)
+	@JsonView(Views.External.class)
 	private String firstname;
 	
 	@Column(name = "LAST_NAME", length = 50, nullable = false)
+	@JsonView(Views.External.class)
 	private String lastname;
 	
 	@Column(name = "EMAIL", length = 50, nullable = false)
+	@JsonView(Views.External.class)
 	private String email;
 	
 	@Column(name = "ROLE", length = 50, nullable = false)
+	@JsonView(Views.Internal.class)
 	private String role;
 	
 	@Column(name = "SSN", length = 50, nullable = false, unique = true)
 	//@JsonIgnore
+	@JsonView(Views.Internal.class)
 	private String ssn;
 	
 	@OneToMany(mappedBy = "user")
+	@JsonView(Views.Internal.class)
 	private List<Order> orders;
 	
 	public User() {
